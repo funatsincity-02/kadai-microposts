1d3bd46de4a93445181e34f2e10497837fa136d7
yoshi.kobo <funatsincity-02@yahoo.co.jp>
2020-09-06 06:31:36 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
 
   def index
@@ -25,10 +25,23 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       flash.now[:danger] = "ユーザーの登録に失敗しました。"
+      #binding.pry
       render :new
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
   
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
   
   def user_params
